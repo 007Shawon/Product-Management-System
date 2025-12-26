a9f592b135566b61b1c89fed3b08cb8d6f60114b
007Shawon <shawon120744@gmail.com>
2025-12-26 18:23:50 +0600

    final implemenation done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,6 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        //$products = Product::paginate(5);
         return view('products.index', compact('products'));
     }
 
